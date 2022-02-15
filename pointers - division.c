29647acc90c19d466863813ea43b49037517dfda
LefterLuci <99748961+LefterLuci@users.noreply.github.com>
2022-02-15 18:18:23 +0200

    Add files via upload

---
+++ pointers - division.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int div(int *a, int *b)
+    {
+        return (*a) / (*b);
+    }
+
+
+
+
+int main ()
+{
+     int x, y;
+
+     scanf("%d", &x);
+     scanf("%d", &y);
+
+     int div(*x,*y);
+
+     printf("%d", x, y);
+
+
+
+
+    return 0;
+}
+mix)
